0fa119c06a81c9a2062ec899929b1d57f31eb5ad
scvstackkk <scv.stack@gmail.com>
2020-02-20 16:37:48 -0600

    Work on todo.service.ts functions

--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -47,32 +47,33 @@ export class TodoService {
     return this.httpClient.get<Todo>(this.todoUrl + '/' + id);
   }
 
-  filterTodos(todos: Todo[], filters: { status?: boolean, contains?: string, owner?: string, category?: string,
+  filterTodos(todos: Todo[], filters: { status?: string, contains?: string, owner?: string, category?: string,
     orderBy?: string, limit?: string  }): Todo[] {
 
     let filteredTodos = todos;
 
     // Filter by status
     if (filters.status) {
-      if (filters.status === true) {
+      if (filters.status === 'complete') {
         filteredTodos = filteredTodos.filter(todo => {
-
-        })
+          return todo.status === true;
+        });
+      } else if (filters.status === 'incomplete') {
+        filteredTodos = filteredTodos.filter(todo => {
+          return todo.status === false;
+        });
+      } else {
+        throw new Error(
+          'Invalid status filter. Use complete or incomplete.'
+        );
       }
-
-      filters.status = filters.status.toLowerCase();
-
-      filteredTodos = filteredTodos.filter(todo => {
-        return todo.status.toLowerCase().indexOf(filters.status) !== -1;
-      });
     }
 
     // Filter by contains
     if (filters.contains) {
       filters.contains = filters.contains.toLowerCase();
-
       filteredTodos = filteredTodos.filter(todo => {
-        return todo.body.toLowerCase().indexOf(filters.contains) !== -1;
+        return todo.body.toLowerCase().includes(filters.contains) === true;
       });
     }
 
